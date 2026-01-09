4afe9d6af28ab1cf1beba93e48a8880eb6b23263
Arcelyth <arcelyth@tutamail.com>
2026-01-09 20:48:11 +0800

    day_6/elixir_part2

---
+++ Day_6/Elixir/part2.exs
@@ -0,0 +1,37 @@
+defmodule LightGrid do
+  def solve do
+    "../input" 
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{}, fn line, grid -> parse_instruction(line, grid) end)
+    |> Map.values()
+    |> Enum.sum()
+    |> IO.inspect()    
+  end
+
+  defp parse_instruction(line, grid) do
+    regex = ~r/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/
+    [_, command, x1, y1, x2, y2] = Regex.run(regex, line)
+
+    range_x = String.to_integer(x1)..String.to_integer(x2)
+    range_y = String.to_integer(y1)..String.to_integer(y2)
+
+    Enum.reduce(range_x, grid, fn x, acc_grid -> 
+      Enum.reduce(range_y, acc_grid, fn y, acc ->
+        handle_light(acc, command, {x, y})
+      end)
+    end)
+  end
+
+  defp handle_light(grid, "turn on", pos) do
+    Map.update(grid, pos, 1, fn v -> v + 1 end)
+  end
+  defp handle_light(grid, "turn off", pos) do 
+    Map.update(grid, pos, 0, fn v -> max(0, v - 1) end)
+  end
+  defp handle_light(grid, "toggle", pos) do
+    Map.update(grid, pos, 2, fn v -> v + 2 end)
+  end
+end
+
+LightGrid.solve()
